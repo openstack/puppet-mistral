8f4d8d977828a76fcf238d4837c91fb1fde6e0ed
Emilien Macchi <emilien@redhat.com>
2015-12-21 20:17:28 +0100

    unit tests: make Keystone_endpoint match service by name/type
    Since a chance in puppet-keystone (1], we now match an endpoint with a
    service name/type.
    )
    [1] http://git.openstack.org/cgit/openstack/puppet-keystone/commit/?id=0a4e06abb0f5b3f324464ff5219d2885816311ce
    
    Closes-Bug: #1528308
    
    Change-Id: Ib8ccfee2e9613c6fafe1890e687bc2d18c3eed4d

--- spec/classes/mistral_keystone_auth_spec.rb
+++ spec/classes/mistral_keystone_auth_spec.rb
@@ -26,13 +26,12 @@ describe 'mistral::keystone::auth' do
       :roles   => ['admin']
     )}
 
-    it { is_expected.to contain_keystone_service('mistral').with(
+    it { is_expected.to contain_keystone_service('mistral::workflow').with(
       :ensure      => 'present',
-      :type        => 'workflow',
       :description => 'Openstack workflow Service'
     ) }
 
-    it { is_expected.to contain_keystone_endpoint('RegionOne/mistral').with(
+    it { is_expected.to contain_keystone_endpoint('RegionOne/mistral::workflow').with(
       :ensure       => 'present',
       :public_url   => "http://127.0.0.1:8989/v2",
       :admin_url    => "http://127.0.0.1:8989/v2",
@@ -48,8 +47,8 @@ describe 'mistral::keystone::auth' do
 
     it { is_expected.to contain_keystone_user('mistraly') }
     it { is_expected.to contain_keystone_user_role('mistraly@services') }
-    it { is_expected.to contain_keystone_service('mistraly') }
-    it { is_expected.to contain_keystone_endpoint('RegionOne/mistraly') }
+    it { is_expected.to contain_keystone_service('mistraly::workflow') }
+    it { is_expected.to contain_keystone_endpoint('RegionOne/mistraly::workflow') }
   end
 
   describe 'when overriding service name' do
@@ -61,8 +60,8 @@ describe 'mistral::keystone::auth' do
 
     it { is_expected.to contain_keystone_user('mistral') }
     it { is_expected.to contain_keystone_user_role('mistral@services') }
-    it { is_expected.to contain_keystone_service('mistral_service') }
-    it { is_expected.to contain_keystone_endpoint('RegionOne/mistral_service') }
+    it { is_expected.to contain_keystone_service('mistral_service::workflow') }
+    it { is_expected.to contain_keystone_endpoint('RegionOne/mistral_service::workflow') }
   end
 
   describe 'when disabling user configuration' do
@@ -76,9 +75,8 @@ describe 'mistral::keystone::auth' do
 
     it { is_expected.not_to contain_keystone_user('mistral') }
     it { is_expected.to contain_keystone_user_role('mistral@services') }
-    it { is_expected.to contain_keystone_service('mistral').with(
+    it { is_expected.to contain_keystone_service('mistral::workflow').with(
       :ensure      => 'present',
-      :type        => 'workflow',
       :description => 'Openstack workflow Service'
     ) }
 
@@ -96,9 +94,8 @@ describe 'mistral::keystone::auth' do
 
     it { is_expected.not_to contain_keystone_user('mistral') }
     it { is_expected.not_to contain_keystone_user_role('mistral@services') }
-    it { is_expected.to contain_keystone_service('mistral').with(
+    it { is_expected.to contain_keystone_service('mistral::workflow').with(
       :ensure      => 'present',
-      :type        => 'workflow',
       :description => 'Openstack workflow Service'
     ) }
 
