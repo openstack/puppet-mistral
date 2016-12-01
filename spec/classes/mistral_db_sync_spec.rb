972b2d319dc02e34bbecb0406c8cbdb5088fb3e2
ZhongShengping <chdzsp@163.com>
2016-12-01 17:17:32 +0800

    Fix spec test for sync
    Change-Id: Id235bdf7ab7d9b97e2681517e723d8a8a70913ad

--- spec/classes/mistral_db_sync_spec.rb
+++ spec/classes/mistral_db_sync_spec.rb
@@ -11,6 +11,8 @@ describe 'mistral::db::sync' do
         :path        => '/usr/bin',
         :user        => 'mistral',
         :refreshonly => 'true',
+        :try_sleep   => 5,
+        :tries       => 10,
         :logoutput   => 'on_failure'
       )
 
