3552f67b6620420e52e654e9a4abf00b7f7b9847
ZhongShengping <chdzsp@163.com>
2017-02-06 16:53:23 +0800

    Fix annotation of control_exchange option
    Change-Id: I1be6b9aae7ac03357441f4ce61435ead0de4dad3

--- manifests/init.pp
+++ manifests/init.pp
@@ -159,7 +159,9 @@
 #   Defaults to $::os_service_default
 #
 # [*control_exchange*]
-#   (Optional)
+#   (Optional) The default exchange under which topics are scoped. May be
+#   overridden by an exchange name specified in the transport_url
+#   option.
 #   Defaults to $::os_service_default
 #
 # [*rpc_response_timeout*]
