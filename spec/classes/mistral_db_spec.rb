68eb72297a102f48490dfc71eac16d7d52f42aaa
ZhongShengping <chdzsp@163.com>
2017-04-10 19:48:42 +0800

    oslo db: check puppet resource instead of actual config in spec
    Change-Id: I9e7a5641cefd8b722119739573673b2618b3e6a1

--- spec/classes/mistral_db_spec.rb
+++ spec/classes/mistral_db_spec.rb
@@ -6,12 +6,14 @@ describe 'mistral::db' do
 
     context 'with default parameters' do
 
-      it { is_expected.to contain_mistral_config('database/connection').with_value('sqlite:////var/lib/mistral/mistral.sqlite').with_secret(true) }
-      it { is_expected.to contain_mistral_config('database/idle_timeout').with_value('<SERVICE DEFAULT>') }
-      it { is_expected.to contain_mistral_config('database/min_pool_size').with_value('<SERVICE DEFAULT>') }
-      it { is_expected.to contain_mistral_config('database/max_retries').with_value('<SERVICE DEFAULT>') }
-      it { is_expected.to contain_mistral_config('database/retry_interval').with_value('<SERVICE DEFAULT>') }
-      it { is_expected.to contain_mistral_config('database/db_max_retries').with_value('<SERVICE DEFAULT>') }
+      it { is_expected.to contain_oslo__db('mistral_config').with(
+        :db_max_retries => '<SERVICE DEFAULT>',
+        :connection     => 'sqlite:////var/lib/mistral/mistral.sqlite',
+        :idle_timeout   => '<SERVICE DEFAULT>',
+        :min_pool_size  => '<SERVICE DEFAULT>',
+        :max_retries    => '<SERVICE DEFAULT>',
+        :retry_interval => '<SERVICE DEFAULT>',
+      )}
 
     end
 
@@ -26,17 +28,19 @@ describe 'mistral::db' do
         }
       end
 
-      it { is_expected.to contain_mistral_config('database/connection').with_value('mysql+pymysql://mistral:mistral@localhost/mistral').with_secret(true) }
-      it { is_expected.to contain_mistral_config('database/idle_timeout').with_value('3601') }
-      it { is_expected.to contain_mistral_config('database/min_pool_size').with_value('2') }
-      it { is_expected.to contain_mistral_config('database/max_retries').with_value('11') }
-      it { is_expected.to contain_mistral_config('database/retry_interval').with_value('11') }
-      it { is_expected.to contain_mistral_config('database/db_max_retries').with_value('-1') }
+      it { is_expected.to contain_oslo__db('mistral_config').with(
+        :db_max_retries => '-1',
+        :connection     => 'mysql+pymysql://mistral:mistral@localhost/mistral',
+        :idle_timeout   => '3601',
+        :min_pool_size  => '2',
+        :max_retries    => '11',
+        :retry_interval => '11',
+      )}
     end
 
     context 'with postgresql backend' do
       let :params do
-        { :database_connection     => 'postgresql://mistral:mistral@localhost/mistral', }
+        { :database_connection => 'postgresql://mistral:mistral@localhost/mistral', }
       end
 
       it 'install the proper backend package' do
@@ -47,7 +51,7 @@ describe 'mistral::db' do
 
     context 'with MySQL-python library as backend package' do
       let :params do
-        { :database_connection     => 'mysql://mistral:mistral@localhost/mistral', }
+        { :database_connection => 'mysql://mistral:mistral@localhost/mistral', }
       end
 
       it { is_expected.to contain_package('python-mysqldb').with(:ensure => 'present') }
@@ -55,7 +59,7 @@ describe 'mistral::db' do
 
     context 'with incorrect database_connection string' do
       let :params do
-        { :database_connection     => 'redis://mistral:mistral@localhost/mistral', }
+        { :database_connection => 'redis://mistral:mistral@localhost/mistral', }
       end
 
       it_raises 'a Puppet::Error', /validate_re/
@@ -63,7 +67,7 @@ describe 'mistral::db' do
 
     context 'with incorrect pymysql database_connection string' do
       let :params do
-        { :database_connection     => 'foo+pymysql://mistral:mistral@localhost/mistral', }
+        { :database_connection => 'foo+pymysql://mistral:mistral@localhost/mistral', }
       end
 
       it_raises 'a Puppet::Error', /validate_re/
@@ -84,7 +88,7 @@ describe 'mistral::db' do
 
     context 'using pymysql driver' do
       let :params do
-        { :database_connection     => 'mysql+pymysql://mistral:mistral@localhost/mistral', }
+        { :database_connection => 'mysql+pymysql://mistral:mistral@localhost/mistral', }
       end
 
       it 'install the proper backend package' do
@@ -99,7 +103,7 @@ describe 'mistral::db' do
 
     context 'with sqlite backend' do
       let :params do
-        { :database_connection     => 'sqlite:///var/lib/mistral/mistral.sqlite', }
+        { :database_connection => 'sqlite:///var/lib/mistral/mistral.sqlite', }
       end
 
       it 'install the proper backend package' do
@@ -125,7 +129,7 @@ describe 'mistral::db' do
 
     context 'using pymysql driver' do
       let :params do
-        { :database_connection     => 'mysql+pymysql://mistral:mistral@localhost/mistral', }
+        { :database_connection => 'mysql+pymysql://mistral:mistral@localhost/mistral', }
       end
 
       it { is_expected.not_to contain_package('db_backend_package') }
