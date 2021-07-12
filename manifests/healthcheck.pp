a2ec0e36d34aa22f32a6ed545389edefe13fbde1
Takashi Kajinami <tkajinam@redhat.com>
2021-07-12 13:18:23 +0900

    Add support for healthcheck middleware options
    Depends-on: https://review.opendev.org/772202
    Depends-on: https://review.opendev.org/800405
    Change-Id: I7b20aa48cb16927211a862e264bb51ae4bb50f42

---
+++ manifests/healthcheck.pp
@@ -0,0 +1,50 @@
+# == Class: mistral::healthcheck
+#
+# Configure oslo_middleware options in healthcheck section
+#
+# == Params
+#
+# [*enabled*]
+#   (Optional) Enable the healthcheck endpoint at /healthcheck.
+#   Defaults to $facts['os_service_default']
+#
+# [*detailed*]
+#   (Optional) Show more detailed information as part of the response.
+#   Defaults to $facts['os_service_default']
+#
+# [*backends*]
+#   (Optional) Additional backends that can perform health checks and report
+#   that information back as part of a request.
+#   Defaults to $facts['os_service_default']
+#
+# [*disable_by_file_path*]
+#   (Optional) Check the presense of a file to determine if an application
+#   is running on a port.
+#   Defaults to $facts['os_service_default']
+#
+# [*disable_by_file_paths*]
+#   (Optional) Check the presense of a file to determine if an application
+#   is running on a port. Expects a "port:path" list of strings.
+#   Defaults to $facts['os_service_default']
+#
+class mistral::healthcheck (
+  $enabled               = $facts['os_service_default'],
+  $detailed              = $facts['os_service_default'],
+  $backends              = $facts['os_service_default'],
+  $disable_by_file_path  = $facts['os_service_default'],
+  $disable_by_file_paths = $facts['os_service_default'],
+) {
+
+  include mistral::deps
+
+  mistral_config {
+    'healthcheck/enabled': value => $enabled,
+  }
+
+  oslo::healthcheck { 'mistral_config':
+    detailed              => $detailed,
+    backends              => $backends,
+    disable_by_file_path  => $disable_by_file_path,
+    disable_by_file_paths => $disable_by_file_paths,
+  }
+}
