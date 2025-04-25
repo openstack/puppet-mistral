e4e9f826e1c679af1248ec863b6950b7e08c4323
Tobias Urdin <tobias.urdin@binero.com>
2025-04-25 10:20:14 +0200

    Add action_providers and legacy_action_providers
    This adds two new classes to configure the config
    groups action_providers and legacy_action_providers
    in the Mistral configuration.
    
    Some config opts were introduced in [1] and others
    already existed and has been added as well.
    
    [1] https://review.opendev.org/c/openstack/mistral/+/944015/3
    
    Change-Id: I6302ff41f1bad8c01defb915077ee621faa05b60
    (cherry picked from commit 64543a028f9b4b7f89e997d0e17a2ef10c5acffb)
    (cherry picked from commit 1184caee232c3e885955b7fd8bbebfb7fb0bfcd9)
    (cherry picked from commit b3d3c11964f5adde2ce308db89677921304b0bc9)

---
+++ manifests/action_providers.pp
@@ -0,0 +1,27 @@
+# == Class: mistral::action_providers
+#
+# Configure the action_providers config section.
+#
+# === Parameters
+#
+# [*allowlist*]
+#   (Optional) Allowlist with actions that is allowed to be
+#   loaded, if empty all actions will be allowed.
+#   Defaults to $facts['os_service_default']
+
+# [*denylist*]
+#   (Optional) Denylist with actions that is not allowed to
+#   be loaded, allowlist takes precedence, if empty all actions
+#   will be allowed.
+#   Defaults to $facts['os_service_default']
+#
+class mistral::action_providers (
+  $allowlist = $facts['os_service_default'],
+  $denylist  = $facts['os_service_default'],
+) {
+
+  mistral_config {
+    'action_providers/allowlist': value => $allowlist;
+    'action_providers/denylist':  value => $denylist;
+  }
+}
